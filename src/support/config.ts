fbb3d8ab84aa1b02e69e237d4d78578cb47d672a
ChrisFrost2 <kampa@interia.pl>
2025-05-16 22:42:24 +0200

    Update config.ts

--- src/support/config.ts
+++ src/support/config.ts
@@ -1,14 +1,13 @@
 import { LaunchOptions } from '@playwright/test';
 const browserOptions: LaunchOptions = {
   slowMo: 0,
-  headless: false,
+  headless: true,
   args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
   firefoxUserPrefs: {
     'media.navigator.streams.fake': true,
     'media.navigator.permission.disabled': true,
   },
 };
-
 export const config = {
   browser: process.env.BROWSER ?? 'chromium',
   browserOptions,
